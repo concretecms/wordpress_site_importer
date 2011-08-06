a7635643f586e1007afc450ef31dcf488d96aca8
jack <jack.lightbody@gmail.com>
2011-08-05 21:18:11 -0300

    translation

--- controller.php
+++ controller.php
@@ -1,12 +1,16 @@
 <?php  defined('C5_EXECUTE') or die(_("Access Denied."));
 
 class WordpressSiteImporterPackage extends Package {
-  protected $pkgDescription = 'Add wordpress import capability to your site';
-  protected $pkgName = "WordPress Site Importer";
   protected $pkgHandle = 'wordpress_site_importer';
-
   protected $appVersionRequired = '5.3.3.1';
   protected $pkgVersion = '1.0';
+		 public function getPackageDescription() {
+			  return t("Add wordpress import capability to your site");
+		 }
+
+		 public function getPackageName() {
+			  return t("WordPress Site Importer");
+		 }
 
 
   public function install() {
@@ -14,7 +18,7 @@ class WordpressSiteImporterPackage extends Package {
 	$pkg = parent::install();
     Loader::model('single_page');
     $single_page = SinglePage::add('/dashboard/wordpress_import', $pkg);
-    $single_page->update(array('cName' => 'WordPress Import', 'cDescription' => 'Import WordPress Sites'));  
+    $single_page->update(array('cName' => t('WordPress Import'), 'cDescription' => t('Import WordPress Sites')));  
 
 	 $import_stuff = SinglePage::add('/dashboard/wordpress_import/site',$pkg);
   }
